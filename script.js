aa910d012aad9ae32784c8b94d5e356e107b458b
hind333 <109863744+hind333@users.noreply.github.com>
2022-08-18 15:03:28 +0300

    تم التعديل

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 function BMI(weight,height){
- let result = weight / (height * height)
+ let result = weight/(height*height)
  return result
 }
-console.log(MBI(5,5))
-function Status(BMI){
-    if(BMI < 18.5) {
+console.log(bmi(70,1.6))
+function Status(bmi){
+    if(bmi < 18.5) {
         return"لديك نقص في الوزن"
-    } else if( BMI >=18.5 && BMI< 25){
+    } else if( bmi >=18.5 && bmi< 25){
         return "وزنك صحي"
-    } else if(BMI >=25) {
+    } else if(bmi >=25) {
         return "لديك زيادة في الوزن"
     }
     }
     function calculate(){
-        let weight = document.getElementById(weight).value
-        let height = document.getElementById(height).value
-        let BMI = BMI(weight,height)
-        let desc = Status(BMI)
-        let div = document.getElementById(result)
-        div.innerText = BMI+ "==" +desc
+        let weight = document.getElementById('weight').value
+        let height = document.getElementById('height').value
+        let bmi = BMI(weight,height)
+        let desc = Status(bmi)
+        let div = document.getElementById('result')
+        div.innerText = bmi + " == " + desc
     }
+    
